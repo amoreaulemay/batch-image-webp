ee5ca94586742a1567f915d30fbb7e055c354180
amoreaulemay <alexandre.moreau-lemay@aml-design.com>
2021-04-25 02:16:08 +0200

    Windows v1.1.5

--- windows/jpeg2webp.ps1
+++ windows/jpeg2webp.ps1
@@ -26,6 +26,13 @@ Function Get-Folder($initialDirectory="")
     }
     return $folder
 }
+Function Get-File($initialDirectory=[Environment]::GetFolderPath('Desktop'))
+{
+    Add-Type -AssemblyName System.Windows.Forms
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = $initialDirectory }
+    $null = $FileBrowser.ShowDialog()
+    return $FileBrowser.FileName
+}
 
 # pragma MAIN PROGRAM
 # Copyright (c) 2021 Alexandre Moreau-Lemay
@@ -33,7 +40,7 @@ Function Get-Folder($initialDirectory="")
 # License : AGPL-3.0 License
 
 Clear-Host
-Write-Host "Batch WebP Converter v1.1.4-beta - Windows (PowerShell)"
+Write-Host "Batch WebP Converter v1.1.5 - Windows (PowerShell)"
 Write-Host "Copyright (c) Alexandre Moreau-Lemay 2021."
 Write-Host "Released under the AGPLv3 License.`n`n`n"
 
@@ -79,7 +86,13 @@ Do {
     $album_desc = Read-Host -Prompt "Enter a description for the album"
 } Until ($album_desc)
 Do {
-    $album_year = Read-Host -Prompt "Enter the year of the album"
+    $album_year_temp = Read-Host -Prompt "Enter the year of the album"
+    if($album_year_temp -match '^[0-9]+$') {
+        $album_year = $album_year_temp
+    }
+    else {
+        Write-Host "Please provide a numerical value!" -ForegroundColor Red
+    }
 } Until ($album_year)
 Do {
     $album_city = Read-Host -Prompt "Enter the city of the album"
@@ -88,33 +101,90 @@ Do {
     $album_coun = Read-Host -Prompt "Enter the country of the album"
 } Until ($album_coun)
 
-# pragma 5. Creating the temporary directory
+# pragma 5. Changing directory
 Set-Location -Path $f
+
+# pragma 6. Album Cover Selection
+Do {
+    $auto_cover = Read-Host -Prompt "Do you wish to set an Album cover? If no, it will be auto generated. [y/n]"
+    switch ($auto_cover.ToLower()) {
+        "y" { 
+            Do {
+                $manual_cover = Get-File -initialDirectory $f
+                if($manual_cover -eq "" -Or $null -eq $manual_cover){
+                    Write-Host "Error: The path is not valid!`n`n" -ForegroundColor Red
+                }
+                else {
+                    if(Test-Path -Path $manual_cover) {
+                        $album_cover = Split-Path $manual_cover -Leaf
+                        $manual_cover_valid = $true
+                    }
+                    else {
+                        Write-Host "Error: The path is not valid!`n`n" -ForegroundColor Red
+                    }
+                }
+            } Until ($manual_cover_valid)
+        }
+        "n" {
+            $album_cover = (Get-Item * -Include *.jpg,*.jpeg | Get-Random).Name
+        }
+        Default {
+            Write-Host "`nSorry, the answer is not valid. Please try again!`n" -ForegroundColor Red
+        }
+    }
+} Until ($album_cover)
+
+# pragma 7. Creating temp folder
 $temp_folder = New-Guid
 New-Item -Name $temp_folder -ItemType "directory" | Out-Null
 New-Item -Name "jpeg" -ItemType "directory" | Out-Null
 New-Item -Name "webp" -ItemType "directory" | Out-Null
 New-Item -Name "originals" -ItemType "directory" | Out-Null
 
-# pragma 6. Moving jpgs to the temporary directory
+# pragma 8. Creating the album.json and populating the start
+New-Item -Name "album.json" -ItemType "file" | Out-Null
+(
+    "{",
+    "    `"name`":`"$album_name`",",
+    "    `"description`":`"$album_desc`",",
+    "    `"year`":$album_year,",
+    "    `"city`":`"$album_city`",",
+    "    `"country`":`"$album_coun`",",
+    "    `"pictures`":["
+) | Out-File -FilePath "album.json" -Append
+
+# pragma 9. Moving jpgs to the temporary directory
 Get-Item *.jpg | Move-Item -Destination $temp_folder | Out-Null
 Get-Item *.jpeg | Move-Item -Destination $temp_folder | Out-Null
 
-# pragma 7. Changing directory to the temporary one
+# pragma 10. Changing directory to the temporary one
 Set-Location -Path $temp_folder
 
-# pragma 8. Renaming, resizing and converting the files
+# pragma 11. Renaming, resizing and converting the files
 
 $convert = "C:\ProgramData\chocolatey\bin\convert.exe"
 
-Write-Host "Converting, this might take a few minutes..."
+$number_of_file = (Get-Item * | Measure-Object).Count
+$i = 1
+
+Clear-Host
 
 foreach ($file in Get-ChildItem) {
-    # $base_name = ($file | Select-Object BaseName).BaseName
+    Write-Progress -Activity "Converting, this might take a few minutes..." -PercentComplete -1
     $extension = ($file | Select-Object Extension).Extension
     
     $guid_name = [System.guid]::NewGuid().toString()
+    if((Split-Path $file -Leaf) -eq $album_cover){
+        $cover = $guid_name
+    }
     $new_file = $guid_name + $extension
+
+    if($i -lt $number_of_file){
+        "        `"$guid_name`"," | Out-File -FilePath ../album.json -Append
+    }
+    else {
+        "        `"$guid_name`"" | Out-File -FilePath ../album.json -Append
+    }
 
     Move-Item -Path $file -Destination $new_file | Out-Null
     & $convert ($new_file,'-resize','2000x2000','-interlace','Plane','-gaussian-blur','0.05','-quality','80','-density','72',"$guid_name-2000$extension")
@@ -126,18 +196,32 @@ foreach ($file in Get-ChildItem) {
     & $convert ($new_file,'-resize','1000x1000','-interlace','Plane','-gaussian-blur','0.05','-quality','80','-density','72','-define','webp:lossless=true',"$guid_name-1000.webp")
     & $convert ($new_file,'-resize','500x500','-interlace','Plane','-gaussian-blur','0.05','-quality','80','-density','72','-define','webp:lossless=true',"$guid_name-500.webp")
     Move-Item -Path $new_file -Destination ../originals/
+    $i = $i + 1
 }
 
-# pragma 9. Moving the files to their final destinations
+# pragma 12. Finish the json file
+(
+    "    ],",
+    "    `"cover`":`"$cover`",",
+    "    `"sizes`":[",
+    "        500,",
+    "        1000,",
+    "        1500,",
+    "        2000",
+    "    ]",
+    "}"
+) | Out-File -FilePath ../album.json -Append
+
+# pragma 13. Moving the files to their final destinations
 Get-Item *.jpg | Move-Item -Destination ../jpeg/ | Out-Null
 Get-Item *.jpeg | Move-Item -Destination ../jpeg/ | Out-Null
 Get-Item *.webp | Move-Item -Destination ../webp/ | Out-Null
 
-# pragma 10. Deleting the temp directory
+# pragma 14. Deleting the temp directory
 Set-Location -Path ../
 Remove-Item $temp_folder -Recurse
 
-# pragma 11. Script Ending
+# pragma 15. Script Ending
 Clear-Host
 Write-Host "Script Executed!" -ForegroundColor Green
 

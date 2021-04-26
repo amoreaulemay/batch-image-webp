64c34fbc2e2623d44019ccb9267f7c222a4a6790
amoreaulemay <alexandre.moreau-lemay@aml-design.com>
2021-04-26 15:12:00 +0200

    Update jpeg2webp.ps1

--- windows/jpeg2webp.ps1
+++ windows/jpeg2webp.ps1
@@ -189,14 +189,17 @@ foreach ($file in Get-ChildItem) {
     }
 
     Move-Item -Path $file -Destination $new_file | Out-Null
+    # JPEGs Resizing
     & $convert ($new_file,'-resize','2000x2000','-interlace','Plane','-gaussian-blur','0.05','-quality','80','-density','72',"$guid_name-2000$extension"); $j++
     & $convert ("$guid_name-2000$extension",'-resize','1500x1500','-interlace','Plane',"$guid_name-1500$extension"); $j++
     & $convert ("$guid_name-1500$extension",'-resize','1000x1000','-interlace','Plane',"$guid_name-1000$extension"); $j++
     & $convert ("$guid_name-1000$extension",'-resize','500x500','-interlace','Plane',"$guid_name-500$extension"); $j++
+
+    # WebP Conversion
     & $convert ("$guid_name-2000$extension",'-define','webp:emulate-jpeg-size=true',"$guid_name-2000.webp"); $j++
-    & $convert ("$guid_name-2000.webp",'-resize','1500x1500','-interlace','Plane',"$guid_name-1500.webp"); $j++
-    & $convert ("$guid_name-1500.webp",'-resize','1000x1000','-interlace','Plane',"$guid_name-1000.webp"); $j++
-    & $convert ("$guid_name-1000.webp",'-resize','500x500','-interlace','Plane',"$guid_name-500.webp"); $j++
+    & $convert ("$guid_name-1500$extension",'-define','webp:emulate-jpeg-size=true',"$guid_name-1500.webp"); $j++
+    & $convert ("$guid_name-1000$extension",'-define','webp:emulate-jpeg-size=true',"$guid_name-1000.webp"); $j++
+    & $convert ("$guid_name-500$extension",'-define','webp:emulate-jpeg-size=true',"$guid_name-500.webp"); $j++
     Move-Item -Path $new_file -Destination ../originals/
     $i = $i + 1
 }
